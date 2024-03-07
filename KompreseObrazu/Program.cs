a37b1daa98e0fa33f990d449e71a46c2749f48e1
DanielDubnicky <dubnickyd@gmail.com>
2024-03-07 14:02:47 +0100

    .

--- KompreseObrazu/Program.cs
+++ KompreseObrazu/Program.cs
@@ -1,13 +1,16 @@
-﻿namespace Komprese
+﻿using MessagingToolkit;
+using MessagingToolkit.QRCode.Codec.Data;
+using System.Drawing;
+
+namespace Komprese
 {
     public class KompreseObrazu{
 
         public static void Main (String [] args){
         
-        
 
         //Cesta k testovacímu souboru
-        String testFilePath = @"C:\Users\MartinKapcala\OneDrive - Vítkovická střední průmyslová škola\Dokumenty\Programování\PG\Komprese-obrazu\KompreseObrazu\CSV\obr1-10.csv";
+        String testFilePath = @"C:\Users\DanielDubnický\github-classroom\Vitkovicka-stredni-prumyslova-skola\cv-komprese-obrazu-v2-DanielDubnicky\KompreseObrazu\CSV\obr1-10.csv";
         
         //vytvoření instance třídy Obrazek
         Obrazek inputCSV = new Obrazek(testFilePath);
@@ -21,9 +24,29 @@
         Console.WriteLine("Počet horizontální řádků {0}",inputCSV.CountSymbolInLine(testFilePath));
 
         inputCSV.vypisImg();
-    }
+
+
+        
+        
+        
+        var file = @"C:\Users\DanielDubnický\github-classroom\Vitkovicka-stredni-prumyslova-skola\cv-komprese-obrazu-v2-DanielDubnicky\KompreseObrazu\CSV\67be03eabd6d041592d435a28c61e835.png";
+        Bitmap bitmap = new Bitmap(file);
+        MessagingToolkit.QRCode.Codec.QRCodeDecoder decoder = new MessagingToolkit.QRCode.Codec.QRCodeDecoder();
+        var text = decoder.Decode(new QRCodeBitmapImage(bitmap));
+        Console.WriteLine(text);
+        
+        
+    
+
+
 
     }
+    // QR Code result 
+
+
+    }
+// QR Code result
+    
     
     
 }
